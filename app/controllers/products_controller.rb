c9dd48d4572517aa743a8632bcd822826fca7c30
benstew <benstewart2008@gmail.com>
2016-03-01 18:22:23 -0500

    Added Paperclip gem for pics

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :price, :serving_size, :size, :family, :goal_id)
+      params.require(:product).permit(:name, :description, :price, :serving_size, :size, :family, :goal_id, :image)
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,17 +2,24 @@
 #
 # Table name: products
 #
-#  id           :integer          not null, primary key
-#  name         :string
-#  description  :text
-#  price        :decimal(, )
-#  serving_size :integer
-#  product_size :integer
-#  family       :string
-#  goal_id      :integer
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
+#  id                 :integer          not null, primary key
+#  name               :string
+#  description        :text
+#  price              :decimal(, )
+#  serving_size       :integer
+#  product_size       :integer
+#  family             :string
+#  goal_id            :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  image_file_name    :string
+#  image_content_type :string
+#  image_file_size    :integer
+#  image_updated_at   :datetime
 #
 
 class Product < ActiveRecord::Base
+  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }, default_url: "http://cf.ltkcdn.net/vegetarian/images/std/121509-300x300-Gold_Standard_Whey_Protein.jpg"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
 end
