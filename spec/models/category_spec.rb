5ae6a908d7758f71f9e0a726056715e168071996
benstew <benstewart2008@gmail.com>
2016-03-11 19:24:54 -0500

    Updated category model tests and integration tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -12,29 +12,34 @@
 require "rails_helper"
 
 RSpec.describe Category, :type => :model do
+  subject(:category) { build(:category)}
+
+  describe 'validation of factory' do
+    it "has a valid factory" do
+      expect(category).to be_valid
+    end
+  end
+
+  describe 'validation of attributes' do
+    it "is invalid without a name" do
+      category.name = nil
+      expect(category).to_not be_valid
+    end
+
+    it "is invalid without a description" do
+      category.description = nil
+      expect(category).to_not be_valid
+    end
+
+    it 'add name and category' do
+      category.name = 'Lose Weight'
+      category.description = 'To Lose Weight'
+      expect(category.name).to eq 'Lose Weight'
+      expect(category.description).to eq 'To Lose Weight'
+    end
+  end
 
 
-it "has a valid factory" do
-   category = create(:category)
-   expect(category).to be_valid
- end
 
- it "add name and category" do
-   category = create(:category)
-  #  expect(Category.count).to eq 1
-   expect(category.name).to eq 'Lose Weight'
-   expect(category.description).to eq 'To Lose Weight'
- end
 
- it "is invalid without a name" do
-   category = create(:category)
-   category.name = nil
-   expect(category).to_not be_valid
- end
-
- it "is invalid without a description" do
-   category = create(:category)
-   category.description = nil
-   expect(category).to_not be_valid
- end
 end
--- spec/requests/user_logins_spec.rb
+++ spec/requests/user_logins_spec.rb
@@ -1,14 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe "UserLogins", type: :request do
-  it "User is able to login to their account" do
-    # Creating a new user factory
-    user = create(:user)
+  let(:user) {create(:user)}
+
+  scenario "takes you to root path after logging in" do
     visit root_path
     click_link "Login"
-    fill_in "Email", :with => user.email
-    fill_in "Password", :with => user.password
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
     click_button "Log in"
     expect(current_path).to eq(root_path)
   end
+
+  scenario "takes you somewhere else after unsuccessful login" do
+    visit root_path
+    click_link "Login"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: '1234'
+    click_button "Log in"
+    expect(current_path).to_not eq(root_path)
+  end
+
+  scenario "displayes welcome user message after successful login" do
+    visit root_path
+    click_link "Login"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+    expect(page).to have_content("Hi #{user.email}")
+  end
+
+  scenario "displayes unsuccessful error message when using incorrect login credentials" do
+    visit root_path
+    click_link "Login"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: '1234'
+    click_button "Log in"
+    expect(page).to have_content("Invalid email or password")
+  end
+
+
+
+
+# test@gmail.com test1234
+
+
 end
